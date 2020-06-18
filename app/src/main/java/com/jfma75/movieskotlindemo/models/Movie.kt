d5ec84f4b6fbc38496e536fb44229ec36670af96
Juan Francisco Miranda Aguilar <jfmiranda75@hotmail.com>
2020-06-18 13:43:18 +0200

    Eliminadas las anotaciones obsoletas.

--- app/src/main/java/com/jfma75/movieskotlindemo/models/Movie.kt
+++ app/src/main/java/com/jfma75/movieskotlindemo/models/Movie.kt
@@ -1,11 +1,3 @@
 package com.jfma75.movieskotlindemo.models
 
-import android.os.Parcelable
-import androidx.compose.Model
-//import com.squareup.moshi.JsonClass
-import kotlinx.android.parcel.Parcelize
-
-@Model
-@Parcelize
-//@JsonClass(generateAdapter = true)
-data class Movie(var id :Long, var name : String, var genre: String, var imageId: Int) : Parcelable
+data class Movie(var id :Long, var name : String, var genre: String, var imageId: Int)
--- app/src/main/java/com/jfma75/movieskotlindemo/models/TicketDate.kt
+++
@@ -1,8 +0,0 @@
-package com.jfma75.movieskotlindemo.models
-
-import androidx.compose.Model
-import java.time.LocalDate
-import java.time.LocalTime
-
-@Model
-data class TicketDate(var date: LocalDate, var time: LocalTime)
